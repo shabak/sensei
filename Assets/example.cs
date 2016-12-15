5a84b8acef2c22769f3a6df1d36cc0a0bb36d748
Nikolay Shabak <nikolay@Nikolays-MBP.lan>
2016-12-15 21:29:48 +0200

    init

---
+++ Assets/example.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class example : MonoBehaviour
+{
+    ProgressBarBehaviour BarBehaviour;
+    [SerializeField] float UpdateDelay = 2f;
+
+    IEnumerator Start ()
+    {
+        BarBehaviour = GetComponent<ProgressBarBehaviour>();
+        while (true)
+        {
+            yield return new WaitForSeconds(UpdateDelay);
+            BarBehaviour.Value = Random.value * 100;
+            print("new value: " + BarBehaviour.Value);
+        }
+    }
+}
